031a3f279834a4e4d35f3530a8eebb261f2ad901
oldkaseb <hamedsocialworks@gmail.com>
2025-08-02 16:22:54 +0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+from handlers import setup_handlers
+
+app = Client("admin_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+setup_handlers(app)
+
+print("ربات آماده اجراست...")
+app.run()
